c48475b86280a89ff3a664ede51de641fcb6d17c
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-20 13:01:01 +0000

    MVP: Add endpoints to the list of posts and a single post by id

--- routes/posts.js
+++ routes/posts.js
@@ -3,4 +3,29 @@ const postDb = require('../data/helpers/postDb');
 
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  postDb.get()
+    .then((posts) => {
+      res.status(200).json(posts);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The posts could not be retrieved.'});
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  postDb.getById(id)
+    .then((post) => {
+      if (!post) {
+        res.status(404).json({ message: 'The post with the specified ID does not exist.'});
+      } else {
+        res.status(200).json(post);
+      }
+    })
+      .catch((error) => {
+        res.status(500).json({ error: 'The post information could not be retrieved.'});
+      });
+});
+
 module.exports = router;
