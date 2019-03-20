4d3d3eb7444b582dd2ea23752cbdede32a789656
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-20 13:53:05 +0000

    MVP: Add endpoints to retrieve user and user's posts

--- routes/users.js
+++ routes/users.js
@@ -14,4 +14,34 @@ router.get('/', (req, res) => {
     });
 });
 
+router.get('/:user_id', (req, res) => {
+  const { user_id } = req.params;
+  userDb.getById(user_id)
+    .then((user) => {
+      if (!user) {
+        res.status(404).json({ message: 'The user with the specified ID does not exist' });
+      } else {
+        res.status(200).json(user);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json({ error: 'The user information could not be retrieved.' });
+    });
+});
+
+router.get('/:user_id/posts', (req, res) => {
+  const { user_id } = req.params;
+  userDb.getUserPosts(user_id)
+      .then((posts) => {
+        if (!posts) {
+          res.status(404).json({ message: 'The user with the specified ID does not have any posts' });
+        } else {
+          res.status(200).json(posts)
+        }
+      })
+      .catch((error) => {
+        res.status(500).json({ error: 'The user posts could not be retrieved.' });
+      });
+});
+
 module.exports = router;
