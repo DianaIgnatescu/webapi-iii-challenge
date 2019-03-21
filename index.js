89b9d32bb4fcd8d5854ffd697b5df39904b25c31
Diana Ignatescu <diana.ignatescu8@gmail.com>
2019-03-21 13:40:10 +0000

    Use environment PORT

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ server.use(express.json());
 server.use('/api/posts', postsRoutes);
 server.use('/api/users', usersRoutes);
 
-server.listen(5000, () => console.log('Listening on port 5000!'));
+server.listen(process.env.PORT || 5000, () => console.log('Listening on port 5000!'));
